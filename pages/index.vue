66a1cc417d8a34161f08c1705da1924801670489
brnkgabriel <brnkgabriel@gmail.com>
2022-12-24 15:37:13 +0100

    fix: remove google ads

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,6 @@
 </template>
 <script setup lang="ts">
 
-useHead({
-    script: [
-      {
-        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3694921149586901",
-        async: true
-      }
-    ]
-})
 </script>
 <style lang="">
     
