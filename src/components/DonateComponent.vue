9131ed1d8bf0b413ab5e3f2d5ea6424e344e001f
brnkgabriel <brnkgabriel@gmail.com>
2023-03-04 11:17:13 +0100

    added home button to donate page

--- src/components/DonateComponent.vue
+++ src/components/DonateComponent.vue
@@ -11,12 +11,18 @@
           class="bg-paystack-500 font-body text-white w-1/2">
           Paystack (&#8358;)
         </NuxtLink>
+        <NuxtLink href="/" target="_blank" :class="btn" class="font-body w-1/2" :style="style">
+          Home
+        </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 const { btn, mainline } = useUi()
+
+const { globalState } = useGlobals()
+const style = computed(() => `background-color:${globalState.value.tool.bg_color};color:white`)
 </script>
 <style lang="">
   
