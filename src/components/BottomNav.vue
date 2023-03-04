a0eb9f47050e6a998b273c22349db6fd72dc3921
brnkgabriel <brnkgabriel@gmail.com>
2023-03-04 10:10:43 +0100

    fixed bottom nav click

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cell bottomnav">
-    <div class="nav">
+    <!-- <div class="nav">
       <AiTools />
       <div class="tabname text-xxs font-semibold">Ai Tools</div>
     </div>
@@ -10,7 +10,7 @@
     </div>
     <div class="nav">
       <AudioVideo />
-      <div class="tabname text-xxs font-semibold">Audiovisual</div>
+      <div class="tabname text-xxs font-semibold">Audio & Video</div>
     </div>
     <div class="nav">
       <Graphics />
@@ -19,6 +19,11 @@
     <div class="nav">
       <Utilities />
       <div class="tabname text-xxs font-semibold">Utilities</div>
+    </div> -->
+    
+    <div class="nav" v-for="(cat, idx) in globalState.categories" :key="idx" @click="selectTools(cat)">
+      <component :is="comp(cat)"></component>
+      <div class="tabname text-xxs font-semibold">{{cat}}</div>
     </div>
   </div>
 </template>
@@ -28,6 +33,29 @@ import Animation from './tabs/Animation.vue';
 import AudioVideo from './tabs/AudioVideo.vue';
 import Graphics from './tabs/Graphics.vue';
 import Utilities from './tabs/Utilities.vue';
+import Coding from './tabs/Coding.vue'
+import { iTool } from '../types';
+
+const { globalState, setSelectedTools } = useGlobals()
+
+watch(globalState, () => console.log("categories are", globalState.value.categories))
+
+const comp = (category: string) => {
+  switch (category) {
+    case "Animation": return Animation
+    case "Audio & Video": return AudioVideo
+    case "Coding": return Coding
+    case "Graphics": return Graphics
+    case "Utility": return Utilities
+    case "AI Tools": return AiTools
+  }
+}
+
+const selectTools = (category: string) => {
+  const selectedTools = globalState.value.tools.filter((tool: iTool) => tool.category === category)
+  setSelectedTools(selectedTools)
+  console.log("clicking selected tools")
+}
 
 </script>
 <style>
