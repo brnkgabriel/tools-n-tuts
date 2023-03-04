636ed4709b760121ad8cbdc4582f5cc47b573539
brnkgabriel <brnkgabriel@gmail.com>
2023-03-04 17:33:47 +0100

    removed the orange blink from thumbnail pane

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="thumbnailpane relative">
     <div v-if="isLoaded" class="search flex justify-start items-center px-2 md:pl-0">
-      <!-- <div class="search-btn absolute" :class="btn" @click="handleSearch">
-        <div class="search-btn-clickable absolute" data-type="search-btn"></div>
-        <div class="search-icon"></div>
-        <div aria-label="spinloader" class="spin-loader"></div>
-      </div> -->
       <input type="text" name="search" autocomplete="off"
         class="pr-[40px] rounded-lg h-[36px] w-full search outline-mainblue-500 bg-white border border-gray-300 text-gray-900 text-sm focus:ring-rcnblue-500 focus:border-rcnblue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rcnblue-500 dark:focus:border-rcnblue-500"
         placeholder="Search for tool" v-model="searchTerm" />
     </div>
     <div v-if="isLoaded" class="-tpane thumbnails -front">
       <Tool class="thumb" v-for="(tool, idx) in globalState?.selectedTools" :key="idx" :tool="tool" @click="handleClick(tool)" />
-    </div>
-    <div aria-label="tuts" class="-tpane w-full h-full bg-mainorange-500 -back">
-      tutorials
     </div>
     <ComboBoxSkeleton v-if="!isLoaded" class="search flex items-center" />
     <div v-if="!isLoaded" class="thumbnails">
