ca2c8216fce266a25907d62d5aafc2491582535f
brnkgabriel <brnkgabriel@gmail.com>
2023-03-04 17:31:48 +0100

    updated the 4 buttons on videopanecomponent

--- src/components/VideopaneComponent.vue
+++ src/components/VideopaneComponent.vue
@@ -5,12 +5,12 @@
       <img v-if="isImage" :src="globalState.tool.image" />
     </div>
     <div class="links">
-      <NuxtLink class="link" :href="globalState.tool.homepage" :target="target" :style="style">
+      <NuxtLink class="link" :href="globalState.tool.homepage" :target="target(globalState.tool)" :style="style">
         <div class="link-txt">homepage</div>
         <Homepage class="link-icn" />
       </NuxtLink>
-      <NuxtLink class="link" :href="aboutHref" :target="target" :style="style">
-        <div class="link-txt">{{ aboutTxt }}</div>
+      <NuxtLink class="link" :href="aHref(globalState.tool)" :target="target(globalState.tool)" :style="style">
+        <div class="link-txt">{{ text(globalState.tool) }}</div>
         <Documentation class="link-icn" />
       </NuxtLink>
       <a v-if="!isHome" class="link" :href="globalState.tool.download_page" target="_blank" :style="style">
@@ -25,6 +25,7 @@
   </div>
 </template>
 <script setup lang="ts">
+import { iTool } from '../types';
 import Documentation from './icons/Documentation.vue';
 import Download from './icons/Download.vue';
 import Homepage from './icons/Homepage.vue';
@@ -37,7 +38,20 @@ const style = computed(() => `background-color:${globalState.value.tool.bg_color
 const isHome = computed(() => globalState.value.tool.type === "home")
 
 const isImage = computed(() => globalState.value.tool.intro_video === "")
-const aboutHref = computed(() => isHome ? '/about' : globalState.value.tool.documentation)
-const target = computed(() => isHome ? '_self' : "_blank")
-const aboutTxt = computed(() => isHome ? "about" : "documentation")
+
+
+const aHref = (tool: iTool) => {
+  const isHome = tool.type === "home"
+  return isHome ? '/about' : tool.documentation 
+}
+
+const target = (tool: iTool) => {
+  const isHome = tool.type === "home"
+  return isHome ? '_self' : "_blank"
+}
+
+const text = (tool: iTool) => {
+  const isHome = tool.type === "home"
+  return isHome ? "about" : "documentation"
+}
 </script>
